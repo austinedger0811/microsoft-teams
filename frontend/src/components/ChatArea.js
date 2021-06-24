6c222fe579a994f4b042ba136bc266b3cae0d38a
Austin M. Edger <ame2194@columbia.edu>
2021-06-24 00:09:39 -0400

    Worked through navbar section

--- frontend/src/components/ChatArea.js
+++ frontend/src/components/ChatArea.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import Typography from '@material-ui/core/Typography'
 import SearchIcon from '@material-ui/icons/Search'
+import Avatar from '@material-ui/core/Avatar'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 
 const ChatArea = () => {
 	return (
 		<Container>
 			<NavBar>
-				<SearchBarContainer>
-					<SearchIcon/>
-					<SearchBar type="search" />
-				</SearchBarContainer>
-
+				<SearchBar type="search" placeholder="Search" />
+				<NavButtonContainer>
+					<SettingsButton />
+					<AvatarIcon />
+				</NavButtonContainer>
 			</NavBar>
 			<Header>
-
+				<AvatarIcon />
+				<Typography variant="h6">Edger, Austin</Typography>
 			</Header>
 			<ChatContainer>
 				
@@ -35,17 +39,41 @@ const NavBar = styled.div`
 	flex-shrink: 0;
 	height: 50px;
 	background-color: #464775;
-	//justify-content: space-between;
+	justify-content: space-between;
 	align-items: center;
+	padding-right: 8px;
 `;
 
-const SearchBarContainer = styled.div`
+const NavButtonContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	width: 70px;
 
+`;
+
+const SettingsButton = styled(MoreHorizIcon)`
+	&& {
+		color: white;
+		cursor: pointer;
+		opacity: 0.8;
+	}
+`;
+
+const AvatarIcon = styled(Avatar)`
+&& {
+		cursor: pointer;
+		height: 32px;
+		width: 32px;
+	}
 `;
 
 const Header = styled.div`
+	display: flex;
 	flex-shrink: 0;
+	align-items: center;
 	height: 60px;
+	padding: 20px;
 	background-color: #F5F5F5;
 	border-bottom: solid 1px #E0E0E0;
 `;
@@ -58,10 +86,8 @@ const ChatContainer = styled.div`
 const SearchBar = styled.input`
 	height: 32px;
 	width: 70%;
+	padding: 8px;
 	border-radius: 2px;
 	border: none;
 	opacity: 0.8;
-	
-
-
 `;
